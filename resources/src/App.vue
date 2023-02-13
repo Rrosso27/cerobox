1ab6010323cd9686c5aed748e147207e82abab97
nicolas romaña rosso <nicolasx1625@gmail.com>
2023-02-13 17:53:46 -0500

    Integración  laravel vue

---
+++ resources/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+        <h1>Lavel 9 + vite</h1>
+        <hr>
+        <h3> {{ title }}</h3>
+
+    </div>
+</template>
+
+<script setup>
+const title = "Laravel Title using vue setup"
+</script>
+
+
+<style scoped>
+div {
+    text-align: center;
+}
+</style>
