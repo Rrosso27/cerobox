7638de982317cd0cd84c16c35ca547e2c46f669b
nicolas romaña rosso <nicolasx1625@gmail.com>
2023-02-13 20:58:47 -0500

    Tabla de cliente y servicio
    Add tabla de clientes y servicios

---
+++ database/migrations/2023_02_14_014848_create_services_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('img');
+            $table->string('type_service');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('observations');
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+};
